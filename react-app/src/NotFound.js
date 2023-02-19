b06adba3dc3ae26a33bb14c9ae1454427104162e
Vasugarg2211 <vasumaangarg@gmail.com>
2023-02-19 19:27:49 +0530

    final touchings

--- react-app/src/NotFound.js
+++ react-app/src/NotFound.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
+
 const NotFound = () => {
     return (
+        
         <div className="not-found">
             <h2>Sorry</h2>
-            <h2>That page can not be found</h2>
-            <Link to="/">back to home page...</Link>
+            <h2 className="error-discription">That page can not be found</h2>
+            <Link className="error" to="/">back to home page...</Link>
         </div>
     );
 }
